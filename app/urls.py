d128919e292459f7466b3cf8c3c5e0f4aa0b8297
PythonKodi <art-expo@mail.ru>
2026-01-30 18:05:52 +0300

    Added goods app, connected catalog & product.html

--- app/urls.py
+++ app/urls.py
@@ -21,4 +21,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls',namespace='main')),
+    path('catalog/', include('goods.urls',namespace='catalog')),
 ]
+
---
+++ goods/views.py
@@ -0,0 +1,9 @@
+import re
+from django.shortcuts import render
+
+def catalog(request):
+    return render(request, 'goods/catalog.html')
+
+def product(request):
+    return render(request, 'goods/product.html')
+    
